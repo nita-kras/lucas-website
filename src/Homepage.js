953a74d630f4f07c20fd1db96897d991e22ec6f1
Nita Krasniqi <psynk8@exmail.nottingham.ac.uk>
2024-06-24 13:50:55 +0100

    Add homepage with sidebar and content

---
+++ src/Homepage.js
@@ -0,0 +1,20 @@
+// src/Homepage.js
+import React from 'react';
+import './Homepage.css';
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="sidebar">
+        <button className="sidebar-button">Homepage</button>
+        <button className="sidebar-button">Info</button>
+        <button className="sidebar-button">Bumhole</button>
+      </div>
+      <div className="content">
+        <h1>HELLO WORLD</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
